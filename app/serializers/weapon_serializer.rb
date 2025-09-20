2eae9e79ebe7bd4cd119b6d9a5fb0143490152f4
Edvinas Montvilas <edvinas.montvilas@gmail.com>
2025-09-20 17:14:06 +0300

    improved concurrency for postman benchmarks

--- app/serializers/weapon_serializer.rb
+++ app/serializers/weapon_serializer.rb
@@ -2,5 +2,4 @@ class WeaponSerializer < ActiveModel::Serializer
   attributes :id, :name, :level_requirement, :stats
   has_one :weapon_type
   has_one :quality
-
 end
--- config/puma.rb
+++ config/puma.rb
@@ -24,9 +24,9 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 10)
 threads threads_count, threads_count
-
+preload_app!
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
 
